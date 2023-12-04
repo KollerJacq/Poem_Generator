c09c03e513e99562799e6c2e3b46600667c2b6b9
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2023-12-04 10:00:24 +0100

    Make some changes for user experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ function generatePoem(event) {
     "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions below. Do not include a title to the poem. Include a Signature with '- ai generated -' in a <strong> Element at the end of the poem.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poemExample");
+  poemElement.innerHTML = `Generating poem about ${instructionsInput.value} for you`;
+
   console.log("Generating poem");
   console.log(`Prompt is ${prompt}`);
   console.log(`Context is ${context}`);
